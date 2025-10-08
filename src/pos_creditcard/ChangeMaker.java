f3d6e683d43153401ecfc47cb3f2ae257776f232
Youssef <1670166@uab.cat>
2025-10-08 16:52:28 +0200

    Cambios finales

--- src/pos_creditcard/ChangeMaker.java
+++ src/pos_creditcard/ChangeMaker.java
@@ -1,10 +1,7 @@
 package pos_creditcard;
 
 import java.util.Map;
-/**
- * Interfaz que define el comportamiento común
- * de cualquier algoritmo de cálculo de cambio.
- */
+
 public interface ChangeMaker {
     Map<Double, Integer> makeChange(double changeAmount, Map<Double, Integer> cashBox);
 }
